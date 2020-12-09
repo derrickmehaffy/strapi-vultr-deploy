d8180082ad206a23ca00d61514ba8231cf587fb2
Derrick Mehaffy <derrickmehaffy@gmail.com>
2020-12-08 20:41:00 -0700

    init terraform

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "ssh_key_id" {
+  value = vultr_ssh_key.strapi_ssh_key.id
+}
+
+output "strapi_server_public_ip" {
+  value = vultr_instance.strapi_server.main_ip
+}
+
+output "strapi_server_private_ip" {
+  value = vultr_instance.strapi_server.internal_ip
+}
+
+output "strapi_database_public_ip" {
+  value = vultr_instance.strapi_database.main_ip
+}
+
+output "strapi_database_private_ip" {
+  value = vultr_instance.strapi_database.internal_ip
+}
