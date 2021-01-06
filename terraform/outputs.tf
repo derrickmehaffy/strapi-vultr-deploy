53866a032853be6f8f361ca377c39280dd37883e
Derrick Mehaffy <derrickmehaffy@gmail.com>
2021-01-05 20:40:59 -0700

    WIP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -36,7 +36,7 @@ output "strapi_fqdn" {
 
 ### The Ansible inventory file
 resource "local_file" "AnsibleInventory" {
-  content = templatefile("inventory.tmpl",
+  content = templatefile("templates/inventory.tmpl",
     {
       strapi-label = vultr_instance.strapi_server.label,
       strapi-id    = vultr_instance.strapi_server.id,
@@ -49,4 +49,36 @@ resource "local_file" "AnsibleInventory" {
     }
   )
   filename = "../ansible/inventory"
+}
+
+### Strapi Application Vars
+resource "local_file" "AnsibleHostVars_Strapi" {
+  content = templatefile("templates/strapi_host_vars.tmpl",
+    {
+      strapi-app-name = vultr_instance.strapi_server.label
+      strapi-url      = element(cloudflare_record.strapi_api_a.*.hostname, 0)
+      db-name         = trimsuffix(vultr_instance.strapi_database.label, "-db")
+    }
+  )
+  filename = "../ansible/host_vars/${vultr_instance.strapi_server.label}/tfvars.yml"
+}
+
+### Strapi Database Vars
+resource "local_file" "AnsibleHostVars_Database" {
+  content = templatefile("templates/database_host_vars.tmpl",
+    {
+      db-name = trimsuffix(vultr_instance.strapi_database.label, "-db")
+    }
+  )
+  filename = "../ansible/host_vars/${vultr_instance.strapi_database.label}/tfvars.yml"
+}
+
+### SSH Key
+resource "local_file" "Ansible_All_SSH" {
+  content = templatefile("templates/ssh_key.tmpl",
+    {
+      public-ssh-key = vultr_ssh_key.strapi_ssh_key.ssh_key
+    }
+  )
+  filename = "../ansible/other_vars/tf_ssh.yml"
 }
